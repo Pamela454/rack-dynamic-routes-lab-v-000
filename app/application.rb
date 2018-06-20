ee3100f2196a2d7aa2a9fb6871a1aef9eaae2776
Pamela Frieze <pfrieze@gmail.com>
2018-06-20 22:05:19 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,16 +3,16 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-binding.pry 
+binding.pry
     if req.path=="/<Item Name>"
-      resp.write Item.price
-    elsif
+      if resp.write Item.price
+      elsif
+        resp.write "Item not found"
+        resp.status = 400
+    else
       req.path !="/<Item Name>"
       resp.write "Route not found"
       resp.status = 404
-    else
-      resp.write "Item not found"
-      resp.status = 400
     end
 
     resp.finish
