841d5493c6a877fc42bb18df1c93ab91d4c61cf4
Pamela Frieze <pfrieze@gmail.com>
2018-06-20 21:53:14 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-
+binding.pry 
     if req.path=="/<Item Name>"
       resp.write Item.price
     elsif
