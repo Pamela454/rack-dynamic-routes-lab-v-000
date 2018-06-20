3c22408f40ab18f095edcf6528cfb7241fe861dd
Pamela Frieze <pfrieze@gmail.com>
2018-06-20 22:46:21 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
     @@items << Item.new("Figs", 3.42)
     @@items << Item.new("Pears", 0.99)
 
-    if req.path=="/items/"
+    if req.path.match "/items/"
       i_name = req.path.split("/items/").last
       if item =@@items.find do |item|
         item.name == i_name
